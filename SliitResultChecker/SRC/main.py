2edfe60e75a37c8427ed14013175417f26eca47c
IT18009446 <IT18009446@my.sliit.lk>
2021-05-24 18:51:57 +0530

    Changes added

--- SliitResultChecker/SRC/main.py
+++ SliitResultChecker/SRC/main.py
@@ -7,22 +7,22 @@ id = Idcreater()
 id.enterFields()
 id.initiate()
 
-if(id.choice == 1):
-	rc = ResultChecker(id.oldNic)
+if id.choice == 1:
+    rc = ResultChecker(id.oldNic)
 else:
-	rc = ResultChecker(id.newNic)
-	
+    rc = ResultChecker(id.newNic)
+
 rc.setDetails()
 
 start = time.time()
 processes = []
 for i in range(10):
-	p = multiprocessing.Process(target=rc.initiate, args=(i,))
-	p.start()
-	processes.append(p)
-	
+    p = multiprocessing.Process(target=rc.initiate, args=(i,))
+    p.start()
+    processes.append(p)
+
 for process in processes:
-	process.join()
+    process.join()
 
 end = time.time()
-print("Finished in "+ str(end-start) +" seconds.")
+print("Finished in " + str(end - start) + " seconds.")
